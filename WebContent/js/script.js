4cf44dc8915cfc712e29713c382fa82430c67d7d
Argragas <Argragas@DESKTOP-DU63UOI>
2018-03-10 22:03:22 +0100

    -ajout d'images -modification JS -ajout plugin collision -modification style

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,50 +1,59 @@
 $(function() {
       var ok = 1;
+      var box = '#joueur',
+      drag = $(".drag"),
+      drop = $(".drop");
+
       function deplace()
       {
-        $('#vr').animate({top: '-=600'}, 2500, 'linear', function(){
-          var vrX = Math.floor(Math.random()*194)+70;
-          var vrY = 800;
+    	  var vitesse = Math.floor((3000-1500)*Math.random())+1500
+        $('#vr').animate({top: '800'}, vitesse, 'linear', function(){
+          var vrX = Math.floor(Math.random()*750);
+          var vrY = 0;
           $('#vr').css('top',vrY);
           $('#vr').css('left',vrX);
           ok = 1;
         });
        /* $('.fond').animate({top: '-=360'}, 1000, 'linear', function(){
           $('.fond').css('top',0);
-          deplace();
-        });*/
+          });*/
+         
       };
 	   
       $(document).keydown(function(e){
         if (e.which == 39)
         {
-          vjX = parseInt($('#vj').css('left'));
-          if (vjX < 280)
-          $('#vj').css('left', vjX+30);
+          vjX = parseInt($('#joueur').css('left'));
+          if (vjX < 800)
+          $('#joueur').css('left', vjX+30);
         }
+        
+        
         if (e.which == 37)
         {
-          vjX = parseInt($('#vj').css('left'));
-          if (vjX > 70)
-            $('#vj').css('left', vjX-30);
+          vjX = parseInt($('#joueur').css('left'));
+          if (vjX > 30)
+            $('#joueur').css('left', vjX-30);
         }
       });
 
       function collision()
       {
-        vjX = parseInt($('#vj').css('left'));
-        vrX = parseInt($('#vr').css('left'));
-        vjY = 10;
-        vrY = parseInt($('#vr').css('top'));
-        if (((vrX > vjX) && (vrX < (vjX+66)) && (vrY > vjY) && (vrY < (vjY+150)) && (ok == 1)) 
-        || ((vjX > vrX) && (vjX < (vrX+66)) && (vrY > vjY) && (vrY < (vjY+150)) && (ok == 1)))
-        {
-          $('#son')[0].play();
-          collision = parseInt($('#info').text()) + 1;
-          $('#info').text(collision);
-	  ok = 0;
-        }
+    	 
+          var collides = drop.overlaps(box);
+          $(box)[collides.hits.length ? $(function() {
+        	  $('#vr').css('top',1000);
+              $('#vr').css('left',1000);
+              
+        	  $('#info').text( parseInt($('#info').text()) + 1)
+        	  
+        	  deplace();
+          }) : $(function() {deplace()}) ];
+
+          ok = 0;
       }
+      
+
       deplace();
-      setInterval(collision, 20);
+      setInterval(collision,10);
     });
